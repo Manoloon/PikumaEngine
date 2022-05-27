50e32a581a368de3aab4c4ff478b9de16f7337a8
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2022-05-27 09:33:21 +0100

    arrancando ECS Template for components

--- Src/ECS/ECS.cpp
+++ Src/ECS/ECS.cpp
@@ -2,3 +2,33 @@
 // Created by Manoloon on 26/05/2022.
 //
 #include "ECS.h"
+
+int Entity::GetId() const
+{
+    return id;
+}
+
+void System::AddEntityToSystem(Entity newEntity)
+{
+
+}
+
+void System::RemoveEntityFromSystem(Entity EntityRef)
+{
+
+}
+
+std::vector<Entity> System::GetEntities() const
+{
+    return entities;
+}
+
+const Signature& System::GetComponentSignature() const
+{
+    return componentSignature;
+}
+
+int Component::GetId() const
+{
+    return id;
+}
--- Src/ECS/ECS.h
+++ Src/ECS/ECS.h
@@ -4,20 +4,66 @@
 
 #ifndef PIKUMAENGINE_ECS_H
 #define PIKUMAENGINE_ECS_H
-class Component
-        {
 
-        };
+#include <bitset>
+#include <vector>
+
+const unsigned int MAX_COMPONENTS =32;
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
+//interface for components
+struct IComponent
+{
+protected:
+    static int nextId;
+};
+
+template<typename TComponent>
+class Component : public IComponent
+{
+    static int GetId()
+    {
+        static auto id = nextId++;
+        return id;
+    }
+};
+
 class Entity
-        {
+{
+    int id;
+public:
+    explicit Entity(int newId) : id(newId)
+    {}
 
-        };
+    int GetId() const;
+};
 class System
-        {
+{
+    Signature componentSignature;
+    std::vector<Entity> entities;
+public:
+    void AddEntityToSystem(Entity newEntity);
 
-        };
+    void RemoveEntityFromSystem(Entity EntityRef);
+
+    std::vector<Entity> GetEntities() const;
+
+    const Signature &GetComponentSignature() const;
+
+    template<typename TComponent>void RequireComponent();
+};
+
 class Registry
-        {
+{
 
-        };
+};
+
+// the impl of a template goes on the Header file.
+template<typename TComponent>
+void System::RequireComponent()
+{
+    const auto componentID = Component<TComponent>::GetId();
+    componentSignature.set(componentID);
+}
+
 #endif //PIKUMAENGINE_ECS_H
