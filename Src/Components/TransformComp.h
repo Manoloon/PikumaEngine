a87ae172aca38bef8f919c23d8d37cc70c9dfc46
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2022-06-11 11:16:06 -0300

    terminado el registry class

--- Src/Components/TransformComp.h
+++ Src/Components/TransformComp.h
@@ -10,5 +10,13 @@ struct TransformComp
             sf::Vector2i position;
             sf::Vector2i scale;
             double rotation;
+
+            explicit TransformComp(sf::Vector2i position=sf::Vector2i (0,0),sf::Vector2i scale =
+                    sf::Vector2i (1,1),double rotation = 0.0f)
+            {
+                this->position = position;
+                this->scale = scale;
+                this->rotation = rotation;
+            }
         };
 #endif //PIKUMAENGINE_TRANSFORMCOMP_H
--- Src/ECS/ECS.h
+++ Src/ECS/ECS.h
@@ -19,6 +19,8 @@ typedef std::bitset<MAX_COMPONENTS> Signature;
 struct IComponent
 {
 protected:
+    //as this is an static function, I need to always initialize the value
+    // this is is in the cpp.
     static int nextId;
 };
 
@@ -109,27 +111,78 @@ class Registry
     std::unordered_map<std::type_index,System*> Systems;
 public:
     // remove entity
-    template <typename T,typename ...TArgs>
-    void AddComponent(Entity entity, TArgs&& ...args);
+    template <typename TComponent,typename ...TArgs>
+        void AddComponent(Entity entity, TArgs&& ...args);
 
-    template<typename T>
-    void RemoveComponent(Entity entity);
-
-    template<typename T>
-    bool HasComponent(Entity entity) const;
-    template<typename T>
-    T& GetComponent(Entity entity) const;
+    template<typename TComponent>
+        void RemoveComponent(Entity entity);
+    template<typename TComponent>
+        bool HasComponent(Entity entity) const;
+    template<typename TComponent>
+        TComponent& GetComponent(Entity entity) const;
     Entity CreateEntity();
     void AddEntityToSystem(Entity entity);
     void Update();
 };
 
+// Templates //
+//every template function should be implement in the header file.
 // the impl of a template goes on the Header file.
 template<typename TComponent>
 void System::RequireComponent()
 {
     const auto componentID = Component<TComponent>::GetId();
     componentSignature.set(componentID);
+}
+
+template<typename TComponent, typename... TArgs>
+void Registry::AddComponent(Entity entity, TArgs &&... args)
+{
+    const auto componentId = Component<TComponent>::GetId();
+    const auto entityId = entity.GetId();
+    // if the new component its not in the component Pool -> resize componentPool and init with
+    // nullptr
+    if(componentId >= componentsPool.size())
+    {
+        componentsPool.resize(componentId + 1, nullptr);
+    }
+    // if the new component don´t exist in the componentPool
+    // create a new one and place it in the ComponentsPool
+    if(!componentsPool[componentId])
+    {
+        // TODO : change to smartptr
+        auto newCompPool = new Pool<TComponent>();
+        componentsPool[componentId] = newCompPool;
+    }
+    // get the pool of components for that component value
+    Pool<TComponent>* newCompPool = Pool<TComponent>(componentsPool[componentId]);
+    // if the entity id is greater than the current size of the pool , resize the pool
+    if(entityId >= newCompPool->GetSize())
+    {
+        newCompPool->Resize(numEntities);
+    }
+    //create a new component of type T and forward the many parameters to the constructor.
+    TComponent newComponent(std::forward<TArgs>(args)...);
+    // add the new component to the pool list, using the identity id as index
+    newCompPool->Set(entityId, newComponent);
+    // change the component signature of the entity and set the component id on the bitset to 1
+    componentSignatures[entityId].set(componentId);
+}
+
+template<typename TComponent>
+void Registry::RemoveComponent(Entity entity)
+{
+    const auto componentId = Component<TComponent>::GetId();
+    const auto entityId = entity.GetId();
+    componentSignatures[entityId].set(componentId,false);
+}
+
+template<typename TComponent>
+bool Registry::HasComponent(Entity entity) const
+{
+    const auto componentId = Component<TComponent>::GetId();
+    const auto entityId = entity.GetId();
+    return componentSignatures[entityId].test(componentId);
 }
 
 #endif //PIKUMAENGINE_ECS_H
