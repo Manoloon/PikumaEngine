8a1d8f10bafa25a7bdd3322843ef6e3ea7f0e494
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-04-04 07:38:57 +0200

    damage to enemies working

--- Src/Systems/SDamage.h
+++ Src/Systems/SDamage.h
@@ -28,11 +28,11 @@ public:
         std::unique_ptr<Entity> a = std::make_unique<Entity>(event.EntityA);
         std::unique_ptr<Entity> b = std::make_unique<Entity>(event.EntityB);
 
-        if(a->BelongToGroup("projectiles") && b->HasTag("player"))
+        if(a->BelongToGroup("projectiles") && !b->HasTag("enemies"))
         {
             OnProjectileHitsOther(a,b);
         }
-        if(b->BelongToGroup("projectiles") && a->HasTag("player"))
+        if(b->BelongToGroup("projectiles") && !a->HasTag("enemies"))
         {
             OnProjectileHitsOther(b,a);
         }
@@ -48,22 +48,22 @@ public:
 
     void OnProjectileHitsOther(std::unique_ptr<Entity>& Projectile, std::unique_ptr<Entity>& Other)
     {
-        // std::unique_ptr<CShootEmitter> projectileComp = std::make_unique<CShootEmitter>(Projectile->GetComponent<CShootEmitter>());
-        // if(projectileComp == nullptr)
-        // {
-        //     return;
-        // }
-        // if(!projectileComp->bIsFriendly)
-        // {
-        //     // auto& LocalHealth = Other.GetComponent<CHealth>();
-        //     // LocalHealth.Health -= projectileComp.damagePercentage;
+        std::unique_ptr<CShootEmitter> projectileComp = std::make_unique<CShootEmitter>(Projectile->GetComponent<CShootEmitter>());
+        if(projectileComp == nullptr)
+        {
+            return;
+        }
+        if(!projectileComp->bIsFriendly)
+        {
+            auto& LocalHealth = Other->GetComponent<CHealth>();
+            LocalHealth.Health -= projectileComp->damagePercentage;
 
-        //     // if(LocalHealth.Health <=0)
-        //     // {
-        //     //     Other.Destroy();
-        //     // }
-        //     // Projectile.Destroy();
-        // }
+            if(LocalHealth.Health <=0)
+            {
+                Other->Destroy();
+            }
+            Projectile->Destroy();
+        }
     }
     void Update()
     {
